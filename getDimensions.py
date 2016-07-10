0228da255d1d56b223c99732d1a60518ded0ba1a
makerGeek <moetez.93@gmail.com>
2016-07-10 14:59:10 +0100

    now printing JSON dimensions

--- getDimensions.py
+++ getDimensions.py
@@ -2,6 +2,7 @@
 import numpy as np
 import cv2
 import sys
+import json
 
 #this is the path to the folder where the image is stored
 imgPath="c:\\wamp\\www\\web-landmark-detection\\"
@@ -13,5 +14,6 @@ imgName=imgPath+imgName
 img=cv2.imread(imgName,0)
 #print img
 height, width = img.shape
-
-print 'h: ',height,"  w: ",width
+dimensions ={'height': height, 'width': width}
+dumped=json.dumps(dimensions)
+print dumped
