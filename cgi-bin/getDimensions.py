51f417df0575f0504b0fa65e98d7e553fcd696c3
makerGeek <moetez.93@gmail.com>
2016-07-11 10:16:17 +0100

    added support for different file names (un-hardcoded)

--- cgi-bin/getDimensions.py
+++ cgi-bin/getDimensions.py
@@ -5,22 +5,20 @@ import sys
 import json
 
 import os, cgi
-
+# print
 query = os.environ.get('QUERY_STRING')
 arguments = cgi.parse_qs(query) if query else {}
 if(arguments):
     imgName=arguments["image"]
-    # print imgName
+    imgName= str(imgName[0])
 
     #this is the path to the folder where the image is stored
-    imgPath="c:\\wamp\\www\\web-landmark-detection\\cgi-bin\\face.jpg"
+    imgPath="c:\\wamp\\www\\web-landmark-detection\\cgi-bin\\"+imgName
     # print imgPath
-    # fullName=imgPath+imgName
-    # print fullName
-    # print "okk"
-    img=cv2.imread(imgPath,0)
 
+    img=cv2.imread(imgPath,0)
     # print img
+
     height, width = img.shape
     dimensions ={'height': height, 'width': width}
     dumped=json.dumps(dimensions)
